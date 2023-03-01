75057ee58d52c39e08e1d8bb3084b0c36ae66934
Dani Funes <funesfuentesdaniel@gmail.com>
2023-03-01 17:09:52 +0100

    Resuelto

---
+++ scripts.js
@@ -0,0 +1,80 @@
+const pregunta = {
+  titulo: '¿A quién ha doblado más veces Constantino Romero?',
+  respuestas: [
+      {
+          label: "Clint Eastwood",
+          id: "clint-eastwood",
+          name: "actor",
+          value: "clint-eastwood"
+      },
+      {
+          label: "James Earl",
+          id: "james-earl",
+          name: "actor",
+          value: "james-earl"
+      },
+      {
+          label: "Roger Moore",
+          id: "roger-moore",
+          name: "actor",
+          value: "roger-moore"
+      },
+      {
+          label: "William Shatner",
+          id: "william-shatner",
+          name: "actor",
+          value: "william-shatner"
+      },
+      {
+          label: "Arnold Schwarzenegger",
+          id: "arnold-schwarzenegger",
+          name: "actor",
+          value: "arnold-schwarzenegger"
+      }
+  ]
+}
+
+function imprimePregunta(pregunta) {
+  let newHTML = "";
+
+  newHTML += imprimeTitulo(pregunta);
+  newHTML += imprimeTodasLasRespuestas(pregunta);
+
+  return newHTML;
+}
+
+function imprimeTitulo(pregunta) {
+  // Put your code here
+  const newTitulo = `<p>${pregunta.titulo}</p>`
+  return newTitulo;
+}
+
+function imprimeTodasLasRespuestas(pregunta) {
+  // Put your code here
+  let cadenaRespuestas = "";
+  for (let i = 0; i < pregunta.respuestas.length; i++) {
+      cadenaRespuestas += imprimeUnaRespuesta(pregunta.respuestas[i]);
+  }
+  return cadenaRespuestas;
+
+}
+
+
+function imprimeUnaRespuesta(respuesta) {
+  // Put your code here
+  return imprimeLabel(respuesta) + imprimeInput(respuesta);
+}
+
+function imprimeLabel(respuesta) {
+  // Put your code here
+  let newLabel = `<label for="${respuesta.id}"> ${respuesta.label} </label>`
+  return newLabel;
+}
+
+function imprimeInput(respuesta) {
+  // Put your code here
+  let newInput = `<input id="${respuesta.id}" name="${respuesta.name}" type="radio" value="${respuesta.value}">`
+  return newInput;
+}
+
+document.getElementById("contenedorPreguntas").innerHTML = imprimePregunta(pregunta);
